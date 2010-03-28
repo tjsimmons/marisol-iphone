a969097ee9edafc37088ed8b1943263aec20d1f5
T.J. Simmons <T.J. Simmons@localhost>
2010-03-28 00:05:29 -0500

    basic class structure added

---
+++ Classes/ShipmentListViewController.h
@@ -0,0 +1,16 @@
+//
+//  ShipmentListViewController.h
+//  Marisol
+//
+//  Created by T.J. Simmons on 3/28/10.
+//  Copyright 2010 T.J. Simmons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ShipmentListViewController : UITableViewController {
+
+}
+
+@end
