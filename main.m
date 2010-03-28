10c8de08b4093e9def58e1fcadee9f0df8619951
T.J. Simmons <T.J. Simmons@localhost>
2010-03-27 23:48:31 -0500

    Initial commit; blank app state

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Marisol
+//
+//  Created by T.J. Simmons on 3/27/10.
+//  Copyright T.J. Simmons 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
