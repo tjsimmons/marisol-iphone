deb45d6b9096369c9ea5935d6614a877a5a7e0c7
T.J. Simmons <T.J. Simmons@localhost>
2010-03-28 00:02:01 -0500

    just testing stuff

---
+++ Classes/MarisolAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  MarisolAppDelegate.h
+//  Marisol
+//
+//  Created by T.J. Simmons on 3/27/10.
+//  Copyright T.J. Simmons 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MarisolAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
