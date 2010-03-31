db993371fa20c2856524fdbbce90e5c2a81b1fda
T.J. Simmons <T.J. Simmons@localhost>
2010-03-31 15:05:02 -0500

    lots of code cleanup

--- Classes/ShipmentDetailViewController.m
+++ Classes/ShipmentDetailViewController.m
@@ -18,16 +18,6 @@
 @synthesize coldStorageLabel;
 @synthesize deliveryDateLabel;
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -41,14 +31,6 @@
 	
 	[shipmentIDString release];
 }
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
 
 - (void)didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
@@ -70,7 +52,6 @@
 
 - (void)dealloc {
 	self.shipment = nil;
-	
 	self.shipmentIDLabel = nil;
 	self.marisolNumLabel = nil;
 	self.coldStorageLabel = nil;
--- Classes/iStatRootViewController.m
+++ Classes/iStatRootViewController.m
@@ -16,30 +16,12 @@
 #pragma mark -
 #pragma mark View Methods
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
- - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
- if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
- // Custom initialization
- }
- return self;
- }
- */
-
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
 	
 	[self.view addSubview: navController.view];
 }
-
-/*
- // Override to allow orientations other than the default portrait orientation.
- - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
- // Return YES for supported orientations
- return (interfaceOrientation == UIInterfaceOrientationPortrait);
- }
- */
 
 - (void)didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
