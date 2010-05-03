a30352958722c77ca84c2e45d03b0310f8cb5f11
T.J. Simmons <T.J. Simmons@localhost>
2010-05-03 11:16:46 -0500

    added in base framework for search tab

--- Classes/SearchViewController.h
+++ Classes/SearchViewController.h
@@ -2,15 +2,26 @@
 //  SearchViewController.h
 //  Marisol
 //
-//  Created by T.J. Simmons on 4/21/10.
+//  Created by T.J. Simmons on 5/3/10.
 //  Copyright 2010 T.J. Simmons. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 
-@interface SearchViewController : UIViewController {
-
+@interface SearchViewController : UITableViewController <UISearchDisplayDelegate, UISearchBarDelegate> {
+	NSMutableArray	*searchList;
+	
+	// as demo'd in Apple's TableSearch example code, saved state of search UI
+	NSString		*savedSearchTerm;
+	NSInteger		savedScopeButtonIndex;
+	BOOL			searchWasActive;
 }
+
+@property (nonatomic, retain) NSMutableArray *searchList;
+
+@property (nonatomic, copy) NSString *savedSearchTerm;
+@property (nonatomic) NSInteger savedScopeButtonIndex;
+@property (nonatomic) BOOL searchWasActive;
 
 @end
