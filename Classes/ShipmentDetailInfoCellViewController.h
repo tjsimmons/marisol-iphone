f464c67eaa9bbada01ab91a94a4c1a1b346d9c59
T.J. Simmons <T.J. Simmons@localhost>
2010-05-27 10:10:12 -0500

    added cell subclass to handle shipment details

---
+++ Classes/ShipmentDetailInfoCellViewController.h
@@ -0,0 +1,19 @@
+//
+//  ShipmentDetailInfoCellViewController.h
+//  Marisol
+//
+//  Created by T.J. Simmons on 5/27/10.
+//  Copyright 2010 T.J. Simmons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ShipmentDetailInfoCellViewController : UITableViewCell {
+	UILabel *nameLabel;
+	UILabel *valueLabel;
+}
+
+@property (nonatomic, retain) UILabel *nameLabel, *valueLabel;
+
+@end
---
+++ Classes/ShipmentDetailInfoCellViewController.m
@@ -0,0 +1,52 @@
+//
+//  ShipmentDetailInfoCellViewController.m
+//  Marisol
+//
+//  Created by T.J. Simmons on 5/27/10.
+//  Copyright 2010 T.J. Simmons. All rights reserved.
+//
+
+#import "ShipmentDetailInfoCellViewController.h"
+
+
+@implementation ShipmentDetailInfoCellViewController
+
+@synthesize nameLabel, valueLabel;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if ((self = [super initWithStyle:style reuseIdentifier:reuseIdentifier])) {
+		UILabel *label1 = [[UILabel alloc] initWithFrame: CGRectMake(20, 3, 264, 21)];
+		UILabel *label2 = [[UILabel alloc] initWithFrame: CGRectMake(20, 20, 264, 21)];
+		
+		self.nameLabel = label1;
+		self.valueLabel = label2;
+		
+		[label1 release];
+		[label2 release];
+		
+		valueLabel.textAlignment = UITextAlignmentRight;
+		
+		[self.contentView addSubview: nameLabel];
+		[self.contentView addSubview: valueLabel];
+    }
+    return self;
+}
+
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+
+- (void)dealloc {
+	self.nameLabel = nil;
+	self.valueLabel = nil;
+	
+    [super dealloc];
+}
+
+
+@end
